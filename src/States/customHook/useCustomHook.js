7583926167bf600ea7679adb5b459ac79a2e50d6
Saad Nadeem <saadnadeemch07@gmail.com>
2025-11-03 23:38:58 +0500

    Custom Hook intro

---
+++ src/States/customHook/useCustomHook.js
@@ -0,0 +1,8 @@
+// custom hook must start with use and format shoudk be js its recomended jsx also work btw
+import React from "react";
+
+const CustomHook = () => {
+  return <div>CustomHook</div>;
+};
+
+export default CustomHook;
